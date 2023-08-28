2d826eea628c45ea0f5ba73ffa353162f3b28abf
Daniel Jackoway <commits@danieljackoway.com>
2023-08-27 22:18:27 -0700

    Ugh bugs. Works now

--- api/quote-timeseries.js
+++ api/quote-timeseries.js
@@ -2,9 +2,9 @@ export default async function handler(request, response) {
   const { symbol } = request.query;
   const API_KEY = process.env.ALPHA_VANTAGE_KEY;
   try {
-    const response = await fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${symbol}&apikey=${API_KEY}`);
-    const responseData = await response.json();
-    response.status(200).json(data);
+    const r = await fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&outputsize=full&symbol=${symbol}&apikey=${API_KEY}`);
+    const rData = await r.text();
+    response.send(rData);
   } catch (error) {
     console.error(error);
     response.status(500).json({ error: 'An error occurred' });
